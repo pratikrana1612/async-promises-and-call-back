40925cc6ed8ddc0b503a269cef2316401238d7c9
Rana Pratik <ranapratik1612@gmail.com>
2022-10-21 19:43:43 +0530

    async and await with error handling

--- async,promises and callbacks/app.js
+++ async,promises and callbacks/app.js
@@ -22,7 +22,7 @@ const setTimer = (duration) =>
   return promise;
 }
 
-function trackUserHandler() {
+async function trackUserHandler() {
   // navigator.geolocation.getCurrentPosition(
   //   posdata=>{
   //     setTimer(2000).then(data => console.log(data,posdata));
@@ -30,20 +30,30 @@ function trackUserHandler() {
   //     console.log(error)
   //   }
   // );
+  // let positionData;
   let positionData;
-  getPosition().then(posdata => {
-    //  console.log(posdata) 
-    positionData = posdata;
-    //  setTimer(2000).then(data => console.log(data));
-    return setTimer(2000);
-    },//err => {console.log(err);}
-  ).catch(err => {
-    console.log(err);
-    return 'on we go..';
-  })
-  .then(data => {
-    console.log(positionData,data);
-  }).catch();
+  let timer;
+  try {
+     positionData = await getPosition();
+     timer= await setTimer(2000);
+  }
+  catch(error){
+    console.log(error);
+  }
+  console.log(positionData,timer);
+  // getPosition().then(posdata => {
+  //   //  console.log(posdata) 
+  //   positionData = posdata;
+  //   //  setTimer(2000).then(data => console.log(data));
+  //   return setTimer(2000);
+  //   },//err => {console.log(err);}
+  // ).catch(err => {
+  //   console.log(err);
+  //   return 'on we go..';
+  // })
+  // .then(data => {
+  //   console.log(positionData,data);
+  // }).catch();
   setTimer(1000).then(() => console.log("It's Finally done ok!"));
   console.log('Getting Position...');
 }
@@ -54,7 +64,7 @@ function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
-
+console.log('There is waiting in the only function not outside the function you can see the message i am printing here!');
 // let result = 0;
 // for(let i=0;i<100000000;i++)
 // {
